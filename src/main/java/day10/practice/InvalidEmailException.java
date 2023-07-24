ff94da2c852b14b0f85981099ab9edc9b04c0cfd
p-r-a-v-e-e-n-k-u-m-a-r-G <118426254+p-r-a-v-e-e-n-k-u-m-a-r-G@users.noreply.github.com>
2023-07-24 12:34:18 +0530

    day11.solved JDBC

--- src/main/java/day10/practice/InvalidEmailException.java
+++ src/main/java/day10/practice/InvalidEmailException.java
@@ -1,5 +1,20 @@
 package day10.practice;
 
-public class InvalidEmailException {
+import 
+class InvalidEmailException extends Exception {
+    public InvalidEmailException(String message) {
+        super(message);
+    }
+}
 
+class EmailValidator {
+    public static boolean isValidEmail(String emailId) throws InvalidEmailException {
+        // TODO Write your logic to validate email. If email is valid, return true. Otherwise, throw InvalidEmailException
+        
+        if () {
+            return true;
+        } else {
+            throw new InvalidEmailException("Invalid Email");
+        }
+    }
 }
--- src/main/java/day10/solved/ValidationException2.java
+++ src/main/java/day10/solved/ValidationException2.java
@@ -1,5 +1,14 @@
 package day10.solved;
 
 public class ValidationException2 {
-
+	public static void main(String[] args) {
+		Task task = new Task();
+//		task.setName("Creating test case"); Commenting this so that exception is thrown
+		
+		try {
+			TaskValidator.validate(task);
+		} catch (ValidationException e) {
+			e.printStackTrace();
+		}
+	}
 }
